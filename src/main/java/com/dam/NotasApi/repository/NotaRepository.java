971678c18d3924d892da8beba4b37282c7090538
Agustin <agustindelgadoestevez@gmail.com>
2025-06-05 16:17:02 +0100

    repositorio de nota hecho

--- src/main/java/com/dam/NotasApi/repository/NotaRepository.java
+++ src/main/java/com/dam/NotasApi/repository/NotaRepository.java
@@ -4,10 +4,14 @@
  */
 package com.dam.NotasApi.repository;
 
-/**
- *
- * @author AlumnadoTarde
- */
-public class NotaRepository {
-    
+import com.tuapp.notasapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
